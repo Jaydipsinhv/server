57d3f71da8b5b7851e615c71ced28e87801df809
Jaydipsinh Vaghela <37183780+Jaydipsinhv@users.noreply.github.com>
2022-05-25 12:51:55 +0530

    Update forgotPassword.js

--- src/controller/authentication/forgotPassword.js
+++ src/controller/authentication/forgotPassword.js
@@ -3,32 +3,28 @@ const { jwtSignForgetPasswordToken } = require('./helper');
 const emailProvider = require('../../lib/email');
 
 const forgetPassword = async (req) => {
-  try {
-    const user = await UserModel.findOne({
-      email: req.body.email,
-      isActive: true,
-      isDeleted: false,
-    });
-    if (!user) {
-      return req.sendResponse(403, 'User does not exists.');
-    }
-
-    await UserModel.findAndUpdate(
-      { _id: user._id },
-      { resetPasswordToken: jwtSignForgetPasswordToken({ email: user.email }) },
-    );
-    const updatedUser = await UserModel.findOne({ _id: user._id })
-      .select({ resetPasswordToken: 1, email: 1 });
-
-    const option = {
-      resetPasswordLink: `${process.env.APP_URL}/reset-password/${updatedUser.resetPasswordToken}`,
-    };
-
-    await emailProvider.sendEmail(updatedUser.email, 'Password reset instructions', 'forgotPassword', option);
-    return req.sendResponse(200, `Email sent successfully to ${user.email}`);
-  } catch (err) {
-    return req.sendResponse(500, err);
+  const user = await UserModel.findOne({
+    email: req.body.email,
+    isActive: true,
+    isDeleted: false,
+  });
+  if (!user) {
+    return req.sendResponse(403, 'User does not exists.');
   }
+
+  await UserModel.findAndUpdate(
+    { _id: user._id },
+    { resetPasswordToken: jwtSignForgetPasswordToken({ email: user.email }) },
+  );
+  const updatedUser = await UserModel.findOne({ _id: user._id })
+    .select({ resetPasswordToken: 1, email: 1 });
+
+  const option = {
+    resetPasswordLink: `${process.env.APP_URL}/reset-password/${updatedUser.resetPasswordToken}`,
+  };
+
+  await emailProvider.sendEmail(updatedUser.email, 'Password reset instructions', 'forgotPassword', option);
+  return req.sendResponse(200, `Email sent successfully to ${user.email}`);
 };
 
 module.exports = forgetPassword;
