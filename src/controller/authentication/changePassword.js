90b7d60bb48730f2e5d32b9b4f21cdebee0459f0
Jaydipsinh Vaghela <37183780+Jaydipsinhv@users.noreply.github.com>
2022-05-25 12:50:39 +0530

    Update changePassword.js

--- src/controller/authentication/changePassword.js
+++ src/controller/authentication/changePassword.js
@@ -1,21 +1,17 @@
 const UserModel = require('../user/user.model');
 
 const changePassword = async (req) => {
-  try {
-    const existingUser = await UserModel.findOne({ _id: req.user._id, isActive: true, isDeleted: false }).select('+password');
-    const { oldPassword, newPassword } = req.body;
-    const isPasswordValid = await UserModel.comparePassword(oldPassword, existingUser.password);
-    if (!isPasswordValid) {
-      return req.sendResponse(403, 'Current password is incorrect.');
-    }
-    existingUser.password = newPassword;
-
-    await existingUser.save();
-
-    return req.sendResponse(200, 'Password changed successfully.');
-  } catch (err) {
-    return req.sendResponse(500, err);
+  const existingUser = await UserModel.findOne({ _id: req.user._id, isActive: true, isDeleted: false }).select('+password');
+  const { oldPassword, newPassword } = req.body;
+  const isPasswordValid = await UserModel.comparePassword(oldPassword, existingUser.password);
+  if (!isPasswordValid) {
+    return req.sendResponse(403, 'Current password is incorrect.');
   }
+  existingUser.password = newPassword;
+
+  await existingUser.save();
+
+  return req.sendResponse(200, 'Password changed successfully.');
 };
 
 module.exports = changePassword;
