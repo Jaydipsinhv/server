b13ba2307af95d2d0d9945b0eb58181c90041b3a
Jaydipsinh Vaghela <37183780+Jaydipsinhv@users.noreply.github.com>
2022-05-25 12:58:43 +0530

    Update get.js

--- src/controller/user/get.js
+++ src/controller/user/get.js
@@ -1,18 +1,14 @@
 const UserModel = require('./user.model');
 
 async function get(req) {
-  try {
-    const existingUser = await UserModel.findOne({ email: req.user.email }).lean();
+  const existingUser = await UserModel.findOne({ email: req.user.email }).lean();
 
-    delete existingUser.password;
-    delete existingUser.createdAt;
-    delete existingUser.updatedAt;
-    delete existingUser.__v;
+  delete existingUser.password;
+  delete existingUser.createdAt;
+  delete existingUser.updatedAt;
+  delete existingUser.__v;
 
-    return req.sendResponse(200, existingUser);
-  } catch (err) {
-    return req.sendResponse(500, err);
-  }
+  return req.sendResponse(200, existingUser);
 }
 
 module.exports = get;
