fd7e4bd3c2b8fa1bb5a1e32c2320dce25cb1a36f
Jaydipsinh Vaghela <37183780+Jaydipsinhv@users.noreply.github.com>
2022-05-25 12:56:10 +0530

    Update get.js

--- src/controller/category/get.js
+++ src/controller/category/get.js
@@ -1,34 +1,30 @@
 const CategoryModel = require('./category.model');
 
 const get = async (req) => {
-  try {
-    const limit = parseInt(req.query.limit || process.env.LIMIT || 10);
-    const skip = ((req.query.page ? (req.query.page - 1) : 0) * limit);
+  const limit = parseInt(req.query.limit || process.env.LIMIT || 10);
+  const skip = ((req.query.page ? (req.query.page - 1) : 0) * limit);
 
-    const filter = {
-      isActive: true,
-      isDeleted: false,
-    };
+  const filter = {
+    isActive: true,
+    isDeleted: false,
+  };
 
-    if (req.query.searching) {
-      const regex = req.query.searching.split(' ').join('|');
-      filter.name = { $regex: regex, $options: '$i' };
-    }
-
-    let sortBy = { _id: -1 };
-    if (req.query.sort && req.query.sort.indexOf(',') >= 0) {
-      const sortArray = req.query.sort.split(',');
-      sortBy = { [sortArray[0]]: sortArray[1] };
-    }
-
-    const count = await CategoryModel.count(filter);
-    const data = await CategoryModel.find(filter)
-      .sort(sortBy).skip(skip).limit(limit);
-
-    req.sendResponse(200, { count, data });
-  } catch (err) {
-    req.sendResponse(500, err);
+  if (req.query.searching) {
+    const regex = req.query.searching.split(' ').join('|');
+    filter.name = { $regex: regex, $options: '$i' };
   }
+
+  let sortBy = { _id: -1 };
+  if (req.query.sort && req.query.sort.indexOf(',') >= 0) {
+    const sortArray = req.query.sort.split(',');
+    sortBy = { [sortArray[0]]: sortArray[1] };
+  }
+
+  const count = await CategoryModel.count(filter);
+  const data = await CategoryModel.find(filter)
+    .sort(sortBy).skip(skip).limit(limit);
+
+  return req.sendResponse(200, { count, data });
 };
 
 module.exports = get;
